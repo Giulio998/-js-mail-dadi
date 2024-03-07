275186a2e38cb5205e3f518df3c8139b44b1ad97
Giulio998 <giulio_favia@hotmail.it>
2024-03-07 14:31:11 +0100

    Gioco dei dadi

--- js/app.js
+++ js/app.js
@@ -14,7 +14,7 @@ function emailCheck() {
         } 
 
     }
-    
+
     verified == true ? console.log("la email è gia presente nell'array") : console.log("la email non è presente nell'array");
     /* if (verified == true) {
         console.log("la email è gia presente nell'array");
@@ -25,3 +25,24 @@ function emailCheck() {
 }
 
 document.getElementById("email-button").addEventListener("click", emailCheck)
+
+
+function diceGame() {
+
+ const resultsContainer = document.getElementById("risultato");   
+
+let playerDice = Math.floor(Math.random() * 6) + 1;
+let computerDice = Math.floor(Math.random() * 6) + 1;
+
+if (playerDice < computerDice) {
+    resultsContainer.append(" HAI PERSO "," PLAYER ", playerDice ," COMPUTER ", computerDice )
+    
+} else if (playerDice > computerDice) {
+    resultsContainer.append(" HAI VINTO "," PLAYER ", playerDice ," COMPUTER ", computerDice )
+} else {
+    resultsContainer.append(" HAI PAREGGIATO "," PLAYER ", playerDice ," COMPUTER ", computerDice )
+}
+
+}
+
+document.getElementById("dice-button").addEventListener("click", diceGame)
